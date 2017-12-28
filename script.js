3e7ee805cba5625a3c8adce6152d1ba29da5c183
sapphire <info@sapphire.sh>
2017-12-28 22:49:00 +0900

    init project

---
+++ script.js
@@ -0,0 +1,39 @@
+let options = [];
+
+document.addEventListener('contextmenu', (e) => {
+	const select = e.target;
+	if(select === undefined) {
+		return;
+	}
+	if(select instanceof HTMLSelectElement === false) {
+		return;
+	}
+
+	if(select.getAttribute('prevColor') === null) {
+		select.setAttribute('prevColor', select.style.backgroundColor);
+	}
+
+	select.addEventListener('change', () => {
+		select.style.backgroundColor = select.getAttribute('prevColor');
+	});
+
+	if(options.length === 0) {
+		options = Array.from(select.options).map((e, i) => {
+			return {
+				'index': i,
+				'match': e.textContent.match(/(?!north|democratic)(korea|한국|대한민국|韓國|大韓民國|韓国|大韓民国|韩国|大韩民国)/i),
+			};
+		}).filter((e) => {
+			return e.match !== null;
+		});
+	}
+
+	if(options.length === 0) {
+		select.style.backgroundColor = '#ffc9c9';
+	}
+	else {
+		const option = options.shift();
+		select.selectedIndex = option.index;
+		select.style.backgroundColor = '#b2f2bb';
+	}
+});
